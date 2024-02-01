3ed0381de4968e7279a7897d90a61d47b85e6d35
farhanashamim <farhana_zohir@yahoo.com>
2024-02-01 10:26:20 -0600

    commit again

---
+++ src/variousConcepts/GitTest2.java
@@ -0,0 +1,18 @@
+package variousConcepts;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		printMe();
+		printMe2();
+
+	}
+	public static void printMe() {
+		
+		System.out.println("git test 2");
+	}
+	public static void printMe2() {
+		System.out.println("git test again");
+	}
+
+}

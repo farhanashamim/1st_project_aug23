268f6492264818de920a280ea136b3612f8ad604
farhanashamim <farhana_zohir@yahoo.com>
2024-02-01 12:04:58 -0600

    commit again

--- src/variousConcepts/GitTest3.java
+++ src/variousConcepts/GitTest3.java
@@ -3,8 +3,18 @@ package variousConcepts;
 public class GitTest3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		printMe1();
+		printMe2();
+		printMe3();
 
 	}
-
+	public static void printMe1() {
+		System.out.println("git test1");
+	}
+	public static void printMe2() {
+		System.out.println("git test2");
+	}
+	public static void printMe3() {
+		System.out.println("git test3");
+	}
 }
